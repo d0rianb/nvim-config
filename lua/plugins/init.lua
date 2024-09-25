7cf63f1460b723fa0cd954e74a8a2c68ad867a29
d0rianb <dorian.beauchesne@icloud.com>
2024-09-25 18:31:46 +0200

    Add more snippets & add emmet plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,6 +37,12 @@ require('lazy').setup {
       },
     },
   },
+  {
+    'olrtg/nvim-emmet',
+    config = function()
+      vim.keymap.set({ 'n', 'v', 'i' }, '<A-Tab>', require('nvim-emmet').wrap_with_abbreviation)
+    end,
+  },
 
   -- { -- Useful plugin to show you pending keybinds.
   --   'folke/which-key.nvim',
@@ -84,7 +90,7 @@ require('lazy').setup {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, js = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
