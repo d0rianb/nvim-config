64f59ccb5c5bc08d128946217297d25807e71674
d0rianb <dorian.beauchesne@icloud.com>
2025-10-04 18:48:09 +0200

    Fix lsp setup not working

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -138,14 +138,6 @@ return {
           html = {},
         },
       },
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      --
       ts_ls = {
         on_attach = function(client, bufnr)
           client.server_capabilities.documentFormattingProvider = true
@@ -181,6 +173,9 @@ return {
     }
 
     require('mason').setup()
+    require('mason-lspconfig').setup {
+      automatic_enable = vim.tbl_keys(servers or {}),
+    }
 
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
@@ -195,17 +190,8 @@ return {
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+    for server_name, config in pairs(servers) do
+      vim.lsp.config(server_name, config)
+    end
   end,
 }
