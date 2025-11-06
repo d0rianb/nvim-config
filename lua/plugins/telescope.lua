f3e27a201f647837a41054014a47d2f08c13f905
d0rianb <dorian.beauchesne@icloud.com>
2025-11-06 11:19:02 +0100

    Update telecopte to main branch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
+-- Telescpe wil be depecrated, consider changing to mini.pick, fzf-lua, snacks.picker
 return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
+  branch = 'master',
   dependencies = {
     'nvim-lua/plenary.nvim',
     {
