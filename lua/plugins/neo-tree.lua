1b59ac73f1d18c3d91092e20171de48641c52651
d0rianb <dorian.beauchesne@icloud.com>
2025-11-11 17:55:59 +0100

    Improve neotree on multi- buffers window

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -75,7 +75,10 @@ return {
       enable_diagnostics = false,
       filesystem = {
         hijack_netrw_behavior = 'open_current',
-        follow_current_file = { enabled = true },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
         use_libuv_file_watcher = true,
         window = {
           mappings = {
