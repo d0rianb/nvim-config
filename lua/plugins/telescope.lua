50378c7667fe580c17a2adaae9b41c9393099e8a
d0rianb <dorian.beauchesne@icloud.com>
2025-11-28 09:13:19 +0100

    Add visual mode to telescope edit

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -78,17 +78,17 @@ return {
     local builtin = require 'telescope.builtin'
     local current_path = vim.fn.expand '%:p:h'
     local file_browser_callback = function() extensions.file_browser.file_browser { path = current_path } end
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>fb', file_browser_callback, { desc = '[F]ile [B]rowser' })
-    vim.keymap.set('n', '<leader>fp', extensions.projects.projects, { desc = 'Search [P]rojects' })
-    vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fb', file_browser_callback, { desc = '[F]ile [B]rowser' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fp', extensions.projects.projects, { desc = 'Search [P]rojects' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fs', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader>f.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set({ 'n', 'v', 'x' }, '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
