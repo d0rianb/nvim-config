a7f937a7a257274982756f71838652f1586097df
d0rianb <dorian.beauchesne@icloud.com>
2026-04-09 13:48:00 +0200

    Update lazygit to use pager & remote nvim. Add diff view

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,23 +1,38 @@
 return {
-  'kdheepak/lazygit.nvim',
-  cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+    config = function()
+      require('telescope').load_extension 'lazygit'
+      -- vim.g.lazygit_floating_window_use_plenary = 0
+      -- vim.g.lazygit_use_neovim_remote = 1
+      vim.env.NVIM = vim.v.servername
+    end,
   },
-  -- optional for floating window border decoration
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
+  {
+    'sindrets/diffview.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    config = function()
+      vim.keymap.set('n', '<leader>gd', '<cmd>DiffviewOpen<cr>', { desc = 'Diff working tree' })
+      vim.keymap.set('n', '<leader>gD', '<cmd>DiffviewClose<cr>', { desc = 'Close diffview' })
+    end,
   },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-  },
-  config = function()
-    require('telescope').load_extension 'lazygit'
-  end,
+  { 'esmuellert/codediff.nvim', cmd = 'CodeDiff' },
 }
